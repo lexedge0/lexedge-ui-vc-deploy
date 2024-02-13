c713d592ace0cbe56da1ce5e786d05a3cb8f3eab
maz <miralizain999@gmail.com>
2024-02-13 10:27:00 -0800

    4xl -> 2xl main name

--- components/ui/brand.tsx
+++ components/ui/brand.tsx
@@ -20,7 +20,7 @@ export const Brand: FC<BrandProps> = ({ theme = "dark" }) => {
         <ChatbotUISVG theme={theme === "dark" ? "dark" : "light"} scale={0.3} />
       </div>
 
-      <div className="text-4xl font-bold tracking-normal">LEXEDGE</div>
+      <div className="text-2xl font-bold tracking-wide">LEXEDGE</div>
     </Link>
   )
 }
