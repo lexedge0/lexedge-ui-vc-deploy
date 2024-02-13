1efdd505e3192c47be5e5282726517ef51f53708
maz <miralizain999@gmail.com>
2024-02-13 15:22:38 -0800

    resize model logo

--- components/icons/openai-svg.tsx
+++ components/icons/openai-svg.tsx
@@ -31,9 +31,9 @@ export const OpenAISVG: FC<OpenAISVGProps> = ({
     */
 
     <svg
-      width={width}
-      height={height}
-      viewBox="0 0 141 132"
+      width={25}
+      height={25}
+      viewBox="0 0 41 41"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
