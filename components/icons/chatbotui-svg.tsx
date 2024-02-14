bb60b9d6d923021db1391dca5a36db186e5faaa4
maz <miralizain999@gmail.com>
2024-02-13 17:55:20 -0800

    testing colors

--- components/icons/chatbotui-svg.tsx
+++ components/icons/chatbotui-svg.tsx
@@ -16,14 +16,14 @@ export const ChatbotUISVG: FC<ChatbotUISVGProps> = ({ theme, scale = 1 }) => {
     >
       <path
         d="M90.2339 45.584H44.6418C25.6804 45.584 10.3114 61.3423 10.3114 80.784C10.3114 100.226 25.6804 115.984 44.6418 115.984H90.2339"
-        stroke={`${theme === "dark" ? "#fff" : "#000"}`}
+        stroke={`${theme === "dark" ? "#ff0000" : "#0000ff"}`}
         stroke-width={21}
         stroke-linecap="round"
         stroke-linejoin="round"
       />
       <path
         d="M42.1406 82.6948L92.729 82.6948C111.69 82.6948 127.059 66.9365 127.059 47.4948C127.059 28.0531 111.69 12.2948 92.729 12.2948L47.1369 12.2948"
-        stroke={`${theme === "dark" ? "#fff" : "#000"}`}
+        stroke={`${theme === "dark" ? "#ff0000" : "#0000ff"}`}
         stroke-width={21}
         stroke-linecap="round"
         stroke-linejoin="round"
