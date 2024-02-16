004a4c92e4a602a4cbd7d3813a08df5b658a2b5b
maz <miralizain999@gmail.com>
2024-02-15 17:34:43 -0800

    remove email redirect, add message

--- app/[locale]/login/page.tsx
+++ app/[locale]/login/page.tsx
@@ -128,7 +128,7 @@ export default async function Login({
       password,
       options: {
         // USE IF YOU WANT TO SEND EMAIL VERIFICATION, ALSO CHANGE TOML FILE
-        emailRedirectTo: `https://chat.lexedge.co/`
+        // emailRedirectTo: `https://chat.lexedge.co/`
       }
     })
 
@@ -137,10 +137,10 @@ export default async function Login({
       return redirect(`/login?message=${error.message}`)
     }
 
-    return redirect("/setup")
+    // return redirect("/setup")
 
     // USE IF YOU WANT TO SEND EMAIL VERIFICATION, ALSO CHANGE TOML FILE
-    // return redirect("/login?message=Check email to continue sign in process")
+    return redirect("/login?message=Check email to continue sign in process")
   }
 
   const handleResetPassword = async (formData: FormData) => {
